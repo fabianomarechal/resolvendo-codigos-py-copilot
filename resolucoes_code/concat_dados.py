2b4d75249df4e480720fb934baa21ec4bfc14007
Fabiano Leite <fabiano.marechal@gmail.com>
2024-04-14 03:13:39 +0000

    desafio 1 - concatenar dados

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,11 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+# Solicitando os dados do usuário
+primeiro_dado = input("Por favor, insira o primeiro dado: ")
+segundo_dado = input("Por favor, insira o segundo dado: ")
+
+# Concatenando os dados recebidos
+dados_concatenados = primeiro_dado + segundo_dado
+
+# Exibindo o resultado da concatenação
+print("Dados concatenados:", dados_concatenados)
